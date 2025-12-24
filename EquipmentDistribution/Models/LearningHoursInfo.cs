6eef98a4b3d62571f411df12c65700bf2af96ec5
jxhrk <183056250+jxhrk@users.noreply.github.com>
2025-12-24 23:44:07 +0300

    add class count and learning hours info

---
+++ EquipmentDistribution/Models/LearningHoursInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EquipmentDistribution.Models;
+
+public class LearningHoursInfo
+{
+    public List<LearningHoursInfoClass> Classes { get; set; }
+}
+
+public class LearningHoursInfoClass
+{
+    public int Year { get; set; }
+    public Dictionary<string, int> Hours { get; set; }
+}
--- EquipmentDistribution/Models/SchoolModel.cs
+++ EquipmentDistribution/Models/SchoolModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EquipmentDistribution.Models;
 
@@ -6,7 +8,19 @@ public class SchoolModel
 {
     public string Name { get; set; }
     
+    public Dictionary<int, int> Classes { get; set; }
+    
     public Dictionary<string, int> Classrooms { get; set; }
     
+    public int MaxClassrooms => Classrooms.Values.Sum();
+    
     public override string ToString() => Name;
+
+    public SchoolModel Clone() => new()
+    {
+        Name = Name,
+        Classes = Classes.ToDictionary(entry => entry.Key, entry => entry.Value),
+        Classrooms = Classrooms.ToDictionary(entry => entry.Key, entry => entry.Value),
+    };
+    
 }
